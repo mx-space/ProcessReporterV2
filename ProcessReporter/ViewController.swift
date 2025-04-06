64b75bea30677a50486cabef2324d0e42742c240
Innei <tukon479@gmail.com>
2025-04-06 14:38:45 +0800

    Initial Commit

---
+++ ProcessReporter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProcessReporter
+//
+//  Created by Innei on 2025/4/6.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
